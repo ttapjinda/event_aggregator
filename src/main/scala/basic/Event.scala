29777fe4ee5e91b6f573c24ec0d02949e995dd57
Tayida Tapjinda <tayida@thinkbluedata.com>
2015-11-03 01:10:49 +0700

    clear code and add object for status

--- src/main/scala/basic/Event.scala
+++ src/main/scala/basic/Event.scala
@@ -1,17 +1,18 @@
 package com.tayida.eventaggregator
 package basic
 
-import org.json4s._
-import org.json4s.native.JsonMethods._
-import org.json4s.native.Serialization.{ read, write }
-import spray.httpx.Json4sSupport
-/* Used to mix in Spray's Marshalling Support with json4s */
-object EventProtocol extends Json4sSupport {
-  implicit def json4sFormats: Formats = DefaultFormats
-}
+import spray.json.{ JsonFormat, DefaultJsonProtocol }
 
 /* Event Class */
 // b. The JSON document should have 2 fields
 // 		i. EventType - A string which determines the type of event e.g. "PageView"
 // 		ii. Timestamp - The time at which the event happened in epoch
 case class Event(EventType: String, Timestamp: Long)
+
+case object EventAdded
+
+case object EventAlreadyExists
+
+object EventProtocol extends DefaultJsonProtocol {
+  implicit val eventFormat = jsonFormat2(Event.apply)
+}
